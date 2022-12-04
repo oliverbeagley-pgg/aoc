28eb7006bfb980e8f6e131e87558e94ee78802f1
Olly Beagley <oliver.beagley@puregym.com>
2022-12-04 11:03:49 +0000

    day4

---
+++ aoc_2022/day04/part1.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+from string import ascii_letters
+from textwrap import dedent
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+priority = {letter: priority for priority, letter in enumerate(ascii_letters, 1)}
+
+
+def compute(input: str) -> int:
+    assignments = input.splitlines()
+
+    n = 0
+    for pair in assignments:
+        first, second = pair.split(",")
+        a, b = (int(val) for val in first.split("-"))
+        c, d = (int(val) for val in second.split("-"))
+
+        if a <= c <= d <= b or c <= a <= b <= d:
+            n += 1
+
+    return n
+
+
+TEST_INPUT = """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+"""
+TEST_INPUT = dedent(TEST_INPUT).strip()
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((TEST_INPUT, 2),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
