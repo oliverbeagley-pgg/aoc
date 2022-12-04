2ca0589e5381080b92021e21cf5ded0b211d646d
Olly Beagley <oliver.beagley@puregym.com>
2022-12-04 15:53:47 +0000

    day7

---
+++ aoc_2021/day07/part1.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+from textwrap import dedent
+
+import pytest
+
+import aoc_utils
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(input: str) -> int:
+    positions = aoc_utils.parse_number_comma(input)
+
+    fuel_costs = (
+        sum(abs(position - destination) for position in positions)
+        for destination in range(min(positions), max(positions) + 1)
+    )
+
+    return min(fuel_costs)
+
+
+TEST_INPUT = """
+    16,1,2,0,4,2,7,1,2,14
+"""
+TEST_INPUT = dedent(TEST_INPUT).strip()
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((TEST_INPUT, 37),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ aoc_2021/day07/part2.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+from textwrap import dedent
+
+import pytest
+
+import aoc_utils
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def triangle_number(n: int) -> int:
+    return n * (n + 1) // 2
+
+
+def compute(input: str) -> int:
+    positions = aoc_utils.parse_number_comma(input)
+
+    fuel_costs = (
+        sum(triangle_number(abs(position - destination)) for position in positions)
+        for destination in range(min(positions), max(positions) + 1)
+    )
+
+    return min(fuel_costs)
+
+
+TEST_INPUT = """
+    16,1,2,0,4,2,7,1,2,14
+"""
+TEST_INPUT = dedent(TEST_INPUT).strip()
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((TEST_INPUT, 168),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
