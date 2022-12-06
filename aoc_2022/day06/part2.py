8223821974984a2972f0017ab19b7985929701fc
Olly Beagley <oliver.beagley@puregym.com>
2022-12-06 06:29:18 +0000

    day6

---
+++ aoc_2022/day06/part2.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+from collections import deque
+from textwrap import dedent
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(input: str) -> int:
+    queue: deque[str] = deque(maxlen=14)
+    for idx, val in enumerate(input.strip(), 1):
+        queue.append(val)
+
+        if len(set(queue)) == queue.maxlen:
+            return idx
+
+    raise AssertionError("Should not run")
+
+
+TEST_INPUT = """
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb
+"""
+TEST_INPUT = dedent(TEST_INPUT).strip()
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((TEST_INPUT, 19),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
