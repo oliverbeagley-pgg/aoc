f63207209d01eeaf555f56c299437a78448cc102
Olly Beagley <oliver.beagley@puregym.com>
2022-12-13 10:20:35 +0000

    day10

---
+++ aoc_2022/day10/part2.py
@@ -0,0 +1,231 @@
+from __future__ import annotations
+
+import argparse
+import os
+from textwrap import dedent
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+LARGEST_CYCLE = 40 * 6
+
+
+def render_pixels(pixels: set[tuple[int, int]]) -> str:
+    x_max = max(x for x, _ in pixels)
+    y_max = max(y for _, y in pixels)
+
+    return "\n".join(
+        "".join("#" if (x, y) in pixels else "." for x in range(x_max + 1))
+        for y in range(y_max + 1)
+    )
+
+
+def compute(input: str) -> str:
+    pixels: set[tuple[int, int]] = set()
+    cycle_counter = 0
+    register = 1
+
+    for line in input.splitlines():
+        instruction_cycles = 1 if line == "noop" else 2
+
+        for i in reversed(range(instruction_cycles)):
+            cycle_counter += 1
+
+            if register - 1 <= (cycle_counter - 1) % 40 <= register + 1:
+                pixels.add(((cycle_counter - 1) % 40, (cycle_counter - 1) // 40))
+
+            if i == 0:
+                if line.startswith("addx"):
+                    _, add = line.split()
+                    register += int(add)
+
+        if instruction_cycles > LARGEST_CYCLE:
+            break
+
+    return render_pixels(pixels)
+
+
+TEST_INPUT = """
+    addx 15
+    addx -11
+    addx 6
+    addx -3
+    addx 5
+    addx -1
+    addx -8
+    addx 13
+    addx 4
+    noop
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx -35
+    addx 1
+    addx 24
+    addx -19
+    addx 1
+    addx 16
+    addx -11
+    noop
+    noop
+    addx 21
+    addx -15
+    noop
+    noop
+    addx -3
+    addx 9
+    addx 1
+    addx -3
+    addx 8
+    addx 1
+    addx 5
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx -36
+    noop
+    addx 1
+    addx 7
+    noop
+    noop
+    noop
+    addx 2
+    addx 6
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx 1
+    noop
+    noop
+    addx 7
+    addx 1
+    noop
+    addx -13
+    addx 13
+    addx 7
+    noop
+    addx 1
+    addx -33
+    noop
+    noop
+    noop
+    addx 2
+    noop
+    noop
+    noop
+    addx 8
+    noop
+    addx -1
+    addx 2
+    addx 1
+    noop
+    addx 17
+    addx -9
+    addx 1
+    addx 1
+    addx -3
+    addx 11
+    noop
+    noop
+    addx 1
+    noop
+    addx 1
+    noop
+    noop
+    addx -13
+    addx -19
+    addx 1
+    addx 3
+    addx 26
+    addx -30
+    addx 12
+    addx -1
+    addx 3
+    addx 1
+    noop
+    noop
+    noop
+    addx -9
+    addx 18
+    addx 1
+    addx 2
+    noop
+    noop
+    addx 9
+    noop
+    noop
+    noop
+    addx -1
+    addx 2
+    addx -37
+    addx 1
+    addx 3
+    noop
+    addx 15
+    addx -21
+    addx 22
+    addx -6
+    addx 1
+    noop
+    addx 2
+    addx 1
+    noop
+    addx -10
+    noop
+    noop
+    addx 20
+    addx 1
+    addx 2
+    addx 2
+    addx -6
+    addx -11
+    noop
+    noop
+    noop
+"""
+
+TEST_OUTPUT = """
+    ##..##..##..##..##..##..##..##..##..##..
+    ###...###...###...###...###...###...###.
+    ####....####....####....####....####....
+    #####.....#####.....#####.....#####.....
+    ######......######......######......####
+    #######.......#######.......#######.....
+"""
+
+TEST_INPUT = dedent(TEST_INPUT).strip()
+TEST_OUTPUT = dedent(TEST_OUTPUT).strip()
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((TEST_INPUT, TEST_OUTPUT),),
+)
+def test(input_s: str, expected: str) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
