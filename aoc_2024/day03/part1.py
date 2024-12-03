cfd3ad44fc235a8ae8ceb277a01a2f9e6cd21fda
Olly Beagley <oliver.beagley@puregym.com>
2024-12-03 09:44:04 +0000

    day 3

---
+++ aoc_2024/day03/part1.py
@@ -0,0 +1,47 @@
+import argparse
+import re
+from pathlib import Path
+
+import pytest
+
+import aoc_utils
+
+INPUT_TXT = Path(__file__).parent / "input.txt"
+
+
+def compute(puzzle_input: str) -> int:
+    pattern = re.compile(r"mul\((\d+),(\d+)\)")
+
+    matches: list[tuple[str, str]] = pattern.findall(puzzle_input)
+
+    return sum(int(a) * int(b) for a, b in matches)
+
+
+TEST_INPUT = """\
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    [
+        (TEST_INPUT, 161),
+    ],
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, aoc_utils.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
